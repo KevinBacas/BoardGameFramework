4dce8b914703fd5bccc2837e3790531551036cf8
Clement Cardi <clement.cardi@ecole.ensicaen.fr>
2014-12-15 11:14:02 +0100

    Add Cards and Deck of Cards (Start)

---
+++ src/GameObjects/Card.java
@@ -0,0 +1,74 @@
+package GameObjects;
+
+public class Card {
+	private String m_color;
+	private int m_value;
+	
+	public Card(String color,int value){
+		m_color = color;
+		m_value = value;
+	}
+	
+	public String getColor(){
+		return m_color;
+	}
+	
+	public int getValue(){
+		return m_value;
+	}
+	
+	public String getValueString(){
+		String s;
+		switch(m_value){
+		case 1:
+			s = "As";
+			break;
+		case 2:
+			s = "Deux";
+			break;
+		case 3:
+			s = "Trois";
+			break;
+		case 4:
+			s = "Quatre";
+			break;
+		case 5:
+			s = "Cinq";
+			break;
+		case 6:
+			s = "Six";
+			break;
+		case 7:
+			s = "Sept";
+			break;
+		case 8:
+			s = "Huit";
+			break;
+		case 9:
+			s = "Neuf";
+			break;
+		case 10:
+			s = "Dix";
+			break;
+		case 11:
+			s = "Valet";
+			break;
+		case 12:
+			s = "Reine";
+			break;
+		case 13:
+			s = "Roi";
+			break;
+		default:
+			s = "Joker";
+		}
+		return s;
+	}
+	
+	public String getString(){
+		if(getValue() == 14)
+			return getValueString();
+		else
+			return getValueString() + " de " + getColor();
+	}
+}
---
+++ src/GameObjects/DeckOfCards.java
@@ -0,0 +1,46 @@
+package GameObjects;
+
+import java.util.HashSet;
+
+//TODO: pickRandomCard(nombre de cartes à piocher), equals(DeckOfCards), constructeur pour cartes pour un joueur
+
+public class DeckOfCards {
+	private HashSet<Card> m_cards;
+	private int m_size;
+	
+	public DeckOfCards(int n){
+		m_size = n;
+		m_cards = new HashSet<Card>(m_size);
+		
+		switch(m_size){
+		case 54: // 54 Cards
+			m_cards.add(new Card("Joker1",14));
+			m_cards.add(new Card("Joker2",14));
+		case 52: // 52 Cards
+			for(int i=2;i<=6;i++){
+				m_cards.add(new Card("Carreau",i));
+				m_cards.add(new Card("Coeur",i));
+				m_cards.add(new Card("Trèfle",i));
+				m_cards.add(new Card("Pique",i));
+			}
+		default: // 32 Cards
+			for(int i=7;i<=13;i++){
+				m_cards.add(new Card("Carreau",i));
+				m_cards.add(new Card("Coeur",i));
+				m_cards.add(new Card("Trèfle",i));
+				m_cards.add(new Card("Pique",i));
+			}
+			m_cards.add(new Card("Carreau",1));
+			m_cards.add(new Card("Coeur",1));
+			m_cards.add(new Card("Trèfle",1));
+			m_cards.add(new Card("Pique",1));
+			break;
+		}
+	}
+	
+	//DeckOfCards pickRandomCard(int n){
+		
+		
+	//}
+	
+}
