138dfb2006e942cbdbc127eba986062c53991eb9
Rafael Garcia <rafaelgarcia094@gmail.com>
2014-12-16 16:29:00 +0100

    Changes to HyeneModel

--- src/Hyene/HyeneModel.java
+++ src/Hyene/HyeneModel.java
@@ -9,19 +9,26 @@ import Framework.Model.GameObjectsModel.GameObjectModel;
 public class HyeneModel extends Model{
 	private final static int NUMBER_SQUARES = 57;
 
-	private ArrayList<GameObjectModel> m_board;
+	private ArrayList<GameObjectModel[]> m_board;
 	
 	public HyeneModel(){
 		super();
 		m_board = new ArrayList<>(NUMBER_SQUARES);
 	}
 	
-	public GameObjectModel getElement(int i){
+	public GameObjectModel[] getElement(int i){
 		return m_board.get(i);
 	}
 	
 	public void setElement(int i, GameObjectModel go){
-		m_board.set(i, go);
+		GameObjectModel[] objArray = m_board.get(i);
+		for (GameObjectModel model: objArray){
+			if (model == null){
+				model = go;
+				break;
+			}
+		}
+		m_board.set(i, objArray);
 	}
 	
 	public void draw(Graphics g){
@@ -30,6 +37,7 @@ public class HyeneModel extends Model{
 
 	@Override
 	public boolean isGameEnded() {
+		//for (int i = 1; i < NUMBER_SQUARES; i++)
 		return false;
 	}
 	
